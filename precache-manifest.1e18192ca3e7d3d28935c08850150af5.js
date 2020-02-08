ac515ef7344ecaa1b65974500c6831f590906af2
simple vlasd <vladkhvo@gmail.com>
2020-02-08 23:36:29 +0300

    Updates

---
+++ precache-manifest.1e18192ca3e7d3d28935c08850150af5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa04cf2df14f4cfe62667b75d771669",
+    "url": "/syllable_for_gradeschooler/index.html"
+  },
+  {
+    "revision": "0947c1a36793dcacddf7",
+    "url": "/syllable_for_gradeschooler/static/css/main.9faac7db.chunk.css"
+  },
+  {
+    "revision": "2da8751f6a828d24b961",
+    "url": "/syllable_for_gradeschooler/static/js/2.7a3881d5.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/syllable_for_gradeschooler/static/js/2.7a3881d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0947c1a36793dcacddf7",
+    "url": "/syllable_for_gradeschooler/static/js/main.1a1d430c.chunk.js"
+  },
+  {
+    "revision": "742a4c9dfa49899c382a",
+    "url": "/syllable_for_gradeschooler/static/js/runtime-main.ae6db052.js"
+  }
+]);
